c7d189e214ab6612c26e89a675c481e58b35f122
sun-yi-long <1848327751@qq.com>
2024-09-09 14:00:41 +0800

    SQL99练习代码，SQL92改名

--- MySQL/DQL/DQL_6连接查询1_SQL92.sql
+++ MySQL/DQL/DQL_6连接查询1_SQL92.sql
@@ -150,7 +150,7 @@ WHERE e.`manager_id`=m.`employee_id`;
 
 
 
-#exercise
+#exercise1
 #1.显示员工表的最大工资，工资平均值
 SELECT MAX(salary),AVG(salary)
 FROM employees;
@@ -172,3 +172,52 @@ WHERE s.id=r.studentNo AND s.gradeId=g.id;
 
 #5.显示当前日期，以及去前后空格，截取子字符串的函数
 SELECT NOW();
+
+
+SELECT * FROM departments;
+#exercise2 连接查询
+#1.显示所有员工的姓名，部门号和部门名称。
+SELECT last_name,e.`department_id`,department_name
+FROM employees e,departments d
+WHERE e.`department_id`=d.`department_id`;
+
+#2.查询 90 号部门员工的 job_id 和 90 号部门的 location_id
+SELECT e.`job_id`,d.`location_id`
+FROM employees e,departments d
+WHERE d.`department_id`=e.`department_id`
+AND e.`department_id`=90;
+
+#3.选择所有有奖金的员工的last_name , department_name , location_id , city
+SELECT last_name,department_name,d.location_id,city
+FROM employees e,departments d,locations l
+WHERE commission_pct IS NOT NULL
+AND e.`department_id`=d.`department_id`
+AND d.`location_id`=l.`location_id`;
+
+#4.选择city在Toronto工作的员工的last_name , job_id , department_id , department_name
+SELECT last_name,e.job_id,e.department_id,department_name
+FROM employees e,departments d,locations l
+WHERE city='Toronto'
+AND e.`department_id`=d.`department_id`
+AND l.`location_id`=d.`location_id`;
+
+#5.查询每个工种、每个部门的部门名、工种名和最低工资
+SELECT job_title,department_name,MIN(salary)
+FROM employees e,jobs j,departments d
+WHERE e.`department_id`=d.`department_id`
+AND e.`job_id`=j.`job_id`
+GROUP BY job_title,department_name;
+
+#6.查询每个国家下的部门个数大于 2 的国家编号
+SELECT country_id,COUNT(*) 部门个数
+FROM locations l,departments d
+WHERE d.`location_id`=l.`location_id`
+GROUP BY country_id
+HAVING COUNT(*)>2;
+
+#7.选择指定员工的姓名，员工号，以及他的管理者的姓名和员工号，结果类似于下面的格式
+#employees Emp# manager Mgr#
+#kochhar   101   king    100
+SELECT e.last_name employees,e.employee_id "Emp#",m.`last_name` manager,m.`employee_id` "Mgr#"
+FROM employees e,employees m
+WHERE e.`manager_id`=m.`employee_id`;
